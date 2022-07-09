4b6e203332f504b635063f1b818baffc0b693fb1
Gustavo Rafael <gustavo-kuhl@hotmail.com>
2022-07-09 14:08:44 -0300

    adiconado comandos para iniciar o serviço

--- bashscript/docker-install.sh
+++ bashscript/docker-install.sh
@@ -6,6 +6,10 @@ cd /tmp && \
 curl -fsSL https://get.docker.com -o get-docker.sh && \
 sh ./get-docker.sh && \
 rm get-docker.sh
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+sudo systemctl start docker.service
+sudo systemctl start containerd.service
 sudo usermod -aG docker $USER
 echo "Instalação concluída..."
 exit
